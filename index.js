c73e7d1c125f1e320fdbc0eca2b229abe76f3dba
merinoalex <118947870+merinoalex@users.noreply.github.com>
2023-01-20 19:17:25 -0600

    Exercise 3.18

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    res.send('<p>Phonebook has info for ' + persons.length + ' people.</p>' + '<p>' + new Date() + '.</p>')
+    Person.find({}).then(persons => {
+        res.send('<p>Phonebook has info for ' + persons.length + ' people.</p>' + '<p>' + new Date() + '.</p>')
+    })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
