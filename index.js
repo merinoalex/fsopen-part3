7f419300c26825d04d654bf90df20cbb9a729c42
merinoalex <118947870+merinoalex@users.noreply.github.com>
2023-01-20 18:54:14 -0600

    Exercise 3.17

--- index.js
+++ index.js
@@ -74,6 +74,21 @@ app.delete('/api/persons/:id', (req, res, next) => {
         .catch(err => next(err))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(err => next(err))
+})
+
 const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: 'Unknown endpoint' })
 }
